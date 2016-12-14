6eb5dba127a56a10b7fc6925d48ef2998a3fa3a3
dev <dev>
2016-12-14 22:14:16 +0700

    check connection in Back stock

--- src/BackStockTask.py
+++ src/BackStockTask.py
@@ -1,9 +1,11 @@
 from vocollect_core.task.task import TaskBase
-from vocollect_core.dialog.functions import prompt_only
+from vocollect_core.dialog.functions import prompt_only, prompt_ready
 from vocollect_core.utilities.localization import itext
 
 from LutOdr import InventoryLut
 from vocollect_lut_odr.receivers import StringField, NumericField
+
+import errno
 
 REQUEST_STOCK = 'requestBackStock'
 QUANTITY_PROMPT = 'quantityPrompt'
@@ -24,12 +26,20 @@ class BackStockTask(TaskBase):
         self.addState(QUANTITY_PROMPT, self.quantity_prompt)
     
     def request_stock(self):
-        if self._queryLut.send(self._location) == 0:
+        err = self._queryLut.send(self._location)
+        
+        if err == 0:
             self._quantity = 0
             for data in self._queryLut.get_data():
                 self._quantity += data['Quantity']
-        else:
+                
+        elif err == errno.ECONNREFUSED:
+            prompt_ready(itext('error.connection.refused'), True)
             self.next_state = REQUEST_STOCK
-    
+        
+        elif err == errno.ETIMEDOUT:
+            prompt_ready(itext('error.connection.timeout'), True)
+            self.next_state = REQUEST_STOCK
+            
     def quantity_prompt(self):
         prompt_only(itext('backstock.quantity', str(self._quantity)))
