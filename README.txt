15a37fe5c453b8bde85f39fb3ae0c2943d4b1fbb
dev <dev>
2016-12-14 22:06:39 +0700

    done the rest

--- README.txt
+++ README.txt
@@ -1,9 +1,9 @@
 1. Add error handling to LUTs
 1.1. [X]
 1.2. [X]
-1.3. []
-1.4. []
-1.5. []
+1.3. [X]
+1.4. [X]
+1.5. [X]
 
 2. Modify the LUT/ODR Server and MockServer
-[]
+[X]
--- test.py
+++ test.py
@@ -5,9 +5,9 @@ from main import main
 from vocollect_lut_odr_test.mock_server import MockServer, BOTH_SERVERS
 server = MockServer(True, 15008, 15009)
 server.set_server_response('5 4 X 3 2,123\r\n\r\n', 'LUTLocation')
-server.set_server_response('5 4 X 3 2,456\r\n\r\n', 'LUTBackStock')
+server.set_server_response('5 4 X 3 2,456\r\n5 4 X 3 2,12\r\n\r\n', 'LUTBackStock')
 server.set_server_response('X', 'ODRCount')
-#server.start_server(BOTH_SERVERS)
+server.start_server(BOTH_SERVERS)
 
 mock_catalyst.environment_properties['SwVersion.Locale'] = 'en_US'
 
